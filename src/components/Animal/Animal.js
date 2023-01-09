976a56cd7a9fc7403ff80851e1f1fed57e83e0b5
Grey Altr <grey.sifr@gmail.com>
2023-01-09 14:35:20 -0800

    background and cow are showing on live server

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
+import Main from '../Main/Main.js';
+import './Animal.css';
+
 export default function Animal() {
-  return <></>;
+  return (
+    <div className="animal">
+      <img src="animals/cow.svg" />
+      <h1></h1>
+    </div>
+  );
 }
